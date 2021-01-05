67b5abcac56db55ed4aff5c14d029844e8f77eca
ljcuriel10 <ljcuriel0729@gmail.com>
2021-01-04 19:53:02 -0800

    Caesar shift first draft

--- Project_Decoder_Ring_1/src/caesar.js
+++ Project_Decoder_Ring_1/src/caesar.js
@@ -1,3 +1,40 @@
-function caesar(input, shift, encode = true) {}
+function getDecodeChar(encodeChar, shift) {
+    let decodeIndex = (encodeChar - 97) - shift;
+    if(decodeIndex < 0){
+        decodeIndex += 26;
+    }
+    return decodeIndex;
+}
+
+function caesar(input, shift, encode = true) {
+    if(!shift || shift === 0 || shift < -25 || shift > 25){
+        return false;
+    }
+    let str = input.toLowerCase();
+    let result = "";
+    if(encode){
+    for(let i = 0; i < str.length; i++){
+        let char = str[i].charCodeAt();
+        if(char >= 97 && char <= 122) {
+            result += String.fromCharCode(((char - 97 + shift) % 26) + 97);
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+} else {
+    for(let i = 0; i < str.length; i++){
+        let char = str[i].charCodeAt();
+        if(char >= 97 && char <= 122) {
+            result += String.fromCharCode((getDecodeChar(char, shift) % 26) + 97);
+        } else {
+            result += str[i];
+        }
+}
+}
+return result;
+}
+
+
 
 module.exports = caesar;
--- Project_Decoder_Ring_1/test/caesar.test.js
+++ Project_Decoder_Ring_1/test/caesar.test.js
@@ -1,1 +1,28 @@
+const caesar = require("../src/caesar");
+const expect = require("chai").expect;
 // Write your tests here!
+
+describe("caesar", () => {
+    it("Should return false if shift value is not present, equal to 0, less than -25, or greater than 25", (
+    ) => {
+        const actual = caesar("thinkful", 99);
+        expect(actual).to.be.false;
+    })
+    it("Spaces should be maintained throughtout, as should other non-alphabetic symbols", () => {
+        const actual = caesar("This is a secret message!", 8);
+        const expected = 'bpqa qa i amkzmb umaaiom!';
+        expect(actual).to.equal(expected);
+
+    })
+    it("Capital letters can be ignored", () => {
+        const actual = caesar("thinkful", -3);
+        const expected = 'qefkhcri' 
+        expect(actual).to.equal(expected);
+    })
+    it("Should wrap around to the front of the alphabet", () => {
+        const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
+        const expected = 'this is a secret message!';
+        expect(actual).to.equal(expected);
+    })
+});
+// a b c d e f g h i j k l m n o p q r s t u v w x y z
